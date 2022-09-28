9506d1b0b9dfdbfa153a8581eff7670cd1723f32
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-09-27 18:06:10 -0700

    added list of possible pw character

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//PW DATA SPACE, list of all possible character?
+var Alphabet = {
+  lowercase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  upwercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+};
+var symble = ["!", "@", "#", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "~"]
+console.log(symble);
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -8,7 +15,29 @@ function writePassword() {
 
   passwordText.value = password;
 
+
+
+
+
+
+
+
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// To store prompt answers for criteria
+function generatePassword() {
+  var pwLength = prompt("How many characters are you looking for?");
+  var pwNum = confirm("Do you want numbers in your pw?");
+  var upperCase = confirm("Do you want Upper Case Letters in your pw?");
+  var lowerCase = confirm("What about them Lower Case Letters?");
+  var specialChar = confirm("Last but not least, special character, yes/no?");
+};
+
+
+
+
