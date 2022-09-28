59420e3eda03ee195346bfeb8ddeb2804a21bacf
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-09-27 22:19:01 -0700

    added prompts and confirmation to Java code. added input validation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var Alphabet = {
   upwercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
 };
 var symble = ["!", "@", "#", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "~"]
-console.log(symble);
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -31,13 +31,40 @@ generateBtn.addEventListener("click", writePassword);
 
 // To store prompt answers for criteria
 function generatePassword() {
-  var pwLength = prompt("How many characters are you looking for?");
-  var pwNum = confirm("Do you want numbers in your pw?");
-  var upperCase = confirm("Do you want Upper Case Letters in your pw?");
-  var lowerCase = confirm("What about them Lower Case Letters?");
-  var specialChar = confirm("Last but not least, special character, yes/no?");
-};
+  var pwLaw = {
+    pwLength: prompt("How many characters are you looking for?"),
+    pwNum: confirm("Do you want numbers in your pw?"),
+    upperCase: confirm("Do you want Upper Case Letters in your pw?"),
+    lowerCase: confirm("What about them Lower Case Letters?"),
+    specialChar: confirm("Last but not least, special character, yes/no?"),
+  };
+  var pwValid = false;
+  console.log(pwLaw.pwLength);
 
+  // Error messages 
+  if (pwLaw.pwLength < 8) {
+    alert("invalid. plz try again with at least 8 characters")
+    console.log(pwLaw.pwLength);
+    console.log(pwLaw);
+  }
+
+  if ((pwLaw.pwNum == false) && (upperCase == false) && (pwLaw.UpperCase == false) && (pwLaw.specialChar == false)) {
+    pwValid = false;
+    alert(`lol enjoy your 'password' with ${pwLaw.pwLength} empty spaces`)
+    console.log("invalid" + pwValid);
+  }
+  else {
+    pwValid = true;
+  };
+
+  // else if (pwLaw.value == [NaN, false, false, false, false]) {
+  //   pwValid = false
+  //   alert(`Here is your 'password' with ${pwLength} spaces`)
+  // }
+  // else {
+  //   pwValid = true
+  // }
+}
 
 
 
